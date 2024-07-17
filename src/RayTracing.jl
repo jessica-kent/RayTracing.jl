58e0acd7e9d36493e2bc32e22d21000ff1be376d
Jessica Kent <jessicajkent1998@gmail.com>
2024-07-17 20:36:33 +0100

    add dependencies, export types and functions

--- src/RayTracing.jl
+++ src/RayTracing.jl
@@ -1,5 +1,50 @@
 module RayTracing
 
-# Write your package code here.
+#types.jl
+export Ray, RayData, BoundaryType, ReflectionType, TransmissionType
+export InitialConditions, Parameters, Domain
+export DomainFields, VelocityGradients, RaySimulation
+
+#utils.jl
+
+export grad_to_field, initialise_fields, image_to_domain
+export get_submatrix, interpolate_field
+
+# sources.jl
+
+export PointSource
+export point_source
+
+# ray_tracing.jl
+
+export initialise_ray, initialise_ray_data, initialise_parameters
+export advance_ray, calculate_ray
+
+# ray_physics
+
+export snells_law, reflect_ray
+
+#plots.jl
+
+export plot_ray
+
+# geometry.jl
+
+export Region, Boundary
+export get_region, get_normals, get_boundary
+
+
+using Plots
+using RecipesBase
+using MultipleScattering
+using LinearAlgebra
+using Images
+
+include("types.jl")
+include("utils.jl")
+include("geometry.jl")
+include("plots.jl")
+include("ray_physics.jl")
+include("ray_tracing.jl")
 
 end
