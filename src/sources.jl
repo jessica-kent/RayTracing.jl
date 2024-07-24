b4a465eb50efe38f4ff74d05a1db3157bd8daa5b
Jessica Kent <jessicajkent1998@gmail.com>
2024-07-24 20:03:52 +0100

    add abstract type Source

--- src/sources.jl
+++ src/sources.jl
@@ -1,8 +1,10 @@
-struct PointSource
+abstract type Source end
+
+struct PointSource <:Source
     position::Vector{Float64}
     amplitude::Float64
     θs::AbstractVector
-    slowness::Vector{Float64}
+    slowness::Float64
 end
 
 function source(s::PointSource)
@@ -11,7 +13,7 @@ function source(s::PointSource)
     θs = s.θs
     directions = [[cos(θ), sin(θ)] for θ in θs]
 
-    rays = [Ray(position, directions[i], amp, s.slowness) for i in 1:length(θs)]
+    rays = [Ray(position, d, amp, s.slowness*d) for d in directions]
 
     return rays
 end
